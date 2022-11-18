f0272758a4dbdb033f44035ff8a9ecdd4edbba72
Katharina Stein <36165516+kastein@users.noreply.github.com>
2022-11-18 22:14:28 +0100

    package handling

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r', encoding='utf-8') as r:
+    requirements = [line.strip() for line in r]
+
+setup(name='recipe_generation',
+      packages=['graph_processing',
+                'amr_processing',
+                'utils'],
+      install_requires=requirements
+      )
