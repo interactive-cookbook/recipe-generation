13029d5d5012d45e1953d4af112ad85b4241a183
Katharina Stein <36165516+kastein@users.noreply.github.com>
2023-02-20 12:38:17 +0100

    clean-up

--- generate_data_set_split.py
+++ generate_data_set_split.py
@@ -1,12 +1,15 @@
 import os
 from pathlib import Path
+import argparse
+from typing import List, Union
+
 from utils.paths import DATA_DIR, ARA_DIR, MODEL_DIR, PROJ_DIR
 from generate_recipe import generate_recipe_different_orderings, generate_recipe_one_ordering
 from graph_processing.recipe_graph import read_graph_from_conllu
-from typing import List, Union
 
+# TODO: document
 
-def generate_ara1(split_file, split_type, config_file, context_len, orderings=None, output_dir=None):
+def generate_specific_split(split_file, split_type, config_file, context_len, orderings, output_dir=None):
     """
 
     :param split_file:
@@ -17,47 +20,6 @@ def generate_ara1(split_file, split_type, config_file, context_len, orderings=No
     :param output_dir:
     :return:
     """
-    assert os.path.join(DATA_DIR).split(os.sep)[-1] == 'data'
-    recipe_names = read_split_file_names(split_file, split_type)
-
-    generate_from_conllu_file(recipe_names=recipe_names,
-                              configuration_file=config_file,
-                              context_len=context_len,
-                              orderings=orderings,
-                              output_dir=output_dir)
-
-
-def generate_ara2(split_file, split_type, config_file, context_len, orderings=None, output_dir=None):
-    """
-
-    :param split_file:
-    :param config_file:
-    :param split_type:
-    :param orderings:
-    :param output_dir:
-    :return:
-    """
-    assert os.path.join(DATA_DIR).split(os.sep)[-1] == 'data_ara2'
-    recipe_names = read_split_file_names(split_file, split_type)
-
-    generate_from_conllu_file(recipe_names=recipe_names,
-                              configuration_file=config_file,
-                              context_len=context_len,
-                              orderings=orderings,
-                              output_dir=output_dir)
-
-
-def generate_ara1_explicit(split_file, split_type, config_file, context_len, orderings=None, output_dir=None):
-    """
-
-    :param split_file:
-    :param config_file:
-    :param split_type:
-    :param orderings:
-    :param output_dir:
-    :return:
-    """
-    assert os.path.join(DATA_DIR).split(os.sep)[-1] == 'data_ara1_explicit'
     recipe_names = read_split_file_names(split_file, split_type)
 
     generate_from_conllu_file(recipe_names=recipe_names,
@@ -89,7 +51,7 @@ def read_split_file_names(split_file, split_type) -> List[str]:
 def generate_from_conllu_file(recipe_names: List[str],
                               configuration_file: Union[str, Path],
                               context_len: int,
-                              orderings: Union[None, List] = None,
+                              orderings: List,
                               output_dir: Union[str, Path] = None):
     """
 
@@ -101,16 +63,6 @@ def generate_from_conllu_file(recipe_names: List[str],
     :return:
     """
     configuration_file = MODEL_DIR / Path(configuration_file)
-    all_possible_orderings = ['top', 'ids', 'pf', 'pf-lf', 'pf-lf-id']
-    if not orderings:
-        ordering_list = all_possible_orderings
-    else:
-        ordering_list = orderings
-        assert not [ord for ord in orderings if not ord in all_possible_orderings]
-    ordering_names = {'top': 'NetworkX Topological Order', 'ids': 'Token ID Ordering', 'pf': 'Path-First Ordering',
-                      'pf-lf': 'Path-First Longest-First Ordering', 'pf-lf-id': 'Path-First Longest-First IDs Ordering'}
-
-
 
     for recipe_name in recipe_names:
         dish = recipe_name.split('_')[:-1]
@@ -125,37 +77,48 @@ def generate_from_conllu_file(recipe_names: List[str],
         else:
             output_file = os.path.join(PROJ_DIR, 'output', recipe_name+'_generated.txt')
 
-        if len(ordering_list) > 1:
+        if len(orderings) > 1:
             generate_recipe_different_orderings(ac_graph=action_graph, configuration_file=configuration_file,
-                                                ordering_list=ordering_list, ordering_names=ordering_names,
+                                                ordering_list=orderings, ordering_names=ordering_names,
                                                 context_len=context_len, output_file=output_file)
         else:
             generate_recipe_one_ordering(ac_graph=action_graph, configuration_file=configuration_file,
-                                         ordering=ordering_list[0], context_len=context_len,
+                                         ordering=orderings[0], context_len=context_len,
                                          output_file=output_file)
 
 
 if __name__=='__main__':
 
-    """
-    generate_ara1_explicit('./final_ara1_split.tsv',
-                            'test',
-                            'model_explicit_config.json',
-                            'context_len'=1
-                            ['pf-lf-id'],
-                            Path('output/output_explicit'))
-    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--split', required=True)
+    parser.add_argument('--type', required=True)
+    parser.add_argument('--config', required=True)
+    parser.add_argument('--cont', required=True)
+    parser.add_argument('--order', required=False)
+    parser.add_argument('--out', required=False)
 
-    generate_ara1(split_file='./final_ara1_split.tsv',
-                  split_type='test',
-                  config_file='model_no_context_config.json',
-                  context_len=0,
-                  orderings=['pf-lf-id'],
-                  output_dir=Path('output/output_no_context'))
+    args = parser.parse_args()
 
-    generate_ara1(split_file='./final_ara1_split.tsv',
-                  split_type='test',
-                  config_file='model_context_config.json',
-                  context_len=1,
-                  orderings=['pf-lf-id'],
-                  output_dir=Path('output/output_context'))
+    # needed for the headers if a csv file with recipes for different traversals gets created
+    ordering_names = {'top': 'NetworkX Topological Order', 'ids': 'Token ID Ordering', 'pf': 'Path-First Ordering',
+                      'pf-lf': 'Path-First Longest-First Ordering', 'pf-lf-id': 'Path-First Longest-First IDs Ordering'}
+
+    ordering = args.order if args.order else 'pf-lf-id'
+    possible_orderings = ['top', 'ids', 'pf', 'pf-lf', 'pf-lf-id']
+    output = args.out if args.out else None
+
+    if ordering == 'all':
+        generate_specific_split(split_file=args.split,
+                                split_type=args.type,
+                                config_file=args.config,
+                                context_len=args.cont,
+                                orderings=possible_orderings,
+                                output_dir=output)
+    else:
+        assert ordering in possible_orderings
+        generate_specific_split(split_file=args.split,
+                                split_type=args.type,
+                                config_file=args.config,
+                                context_len=args.cont,
+                                orderings=[ordering],
+                                output_dir=output)
